9b9e8be6bddf5d07a8e0ea9ef135502cf2a768ca
TwisT404 <cedric.tanghe1303@gmail.com>
2026-07-16 18:47:19 +0200

    fix transition and name function

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -2,17 +2,16 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../componants/NavBar";
 import "../style/global.css";
 
-function Home() {
+function App() {
 
   return (
     <>
     <main>
         <NavBar />
         <Outlet />
-
     </main>    
     </>
   );
 }
 
-export default Home
+export default App
