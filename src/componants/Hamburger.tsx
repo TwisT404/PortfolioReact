c09574b38c9f50ab5de75d49d5932ab11aa1dc03
TwisT404 <cedric.tanghe1303@gmail.com>
2026-07-16 20:19:12 +0200

    new mobile nav bar

---
+++ src/componants/Hamburger.tsx
@@ -0,0 +1,16 @@
+import "./../style/hamburger.css"
+export default function Hamburger({ onClick, open }: { onClick: () => void; open: boolean }) {
+    return (
+        <div
+            className={`hamburger ${open ? "open" : ""}`}
+            onClick={onClick}
+            role="button"
+            aria-expanded={open}
+            aria-label={open ? "Close menu" : "Open menu"}
+        >
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
--- src/componants/NavBar.tsx
+++ src/componants/NavBar.tsx
@@ -2,9 +2,11 @@ import { useState,  useEffect } from "react";
 import logo from "../assets/Logo.jpg";
 import "../style/navbar.css";   
 import { FaMoon, FaSun  } from "react-icons/fa";
+import Hamburger from "./Hamburger";
 
 function NavBar() {
     const [dark, setDark] = useState(true);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
       useEffect(() => {
             document.documentElement.classList.toggle("dark", dark);
         }, [dark]);
@@ -22,6 +24,21 @@ function NavBar() {
             <button className="dark-mode-toggle" onClick={() => setDark(!dark)}>
                 {dark ? <FaSun /> : <FaMoon />}
             </button>
+        </nav>
+
+        <div className="navbar-hamburger">
+             <button className="dark-mode-toggle" onClick={() => setDark(!dark)}>
+                {dark ? <FaSun /> : <FaMoon />}
+            </button>
+            <Hamburger onClick={() => setIsMenuOpen(!isMenuOpen)} open={isMenuOpen} />
+        </div>
+        <nav className={`navbar-menu ${isMenuOpen ? "open" : ""}`}>
+            <ul>
+                <li><a href="/ ">Home</a></li>
+                <li><a href="/tools">Outils</a></li>
+                <li><a href="/projects">Projets</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
         </nav>
     </header>
     </>
